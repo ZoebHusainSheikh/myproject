a56969acb51ebc11bd499bc7921cd71693fa512a
Zoeb Sheikh <zsheikh@isystango.com>
2017-01-05 17:39:41 +0530

    commit 2

--- MyProject/MyProject/ViewController.m
+++ MyProject/MyProject/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AdViewController.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSBundle *bundle = [NSBundle bundleWithURL:[[NSBundle mainBundle] URLForResource:@"MyFrameworkBundle" withExtension:@"bundle"]];
+    AdViewController *adView = [[AdViewController alloc] initWithNibName:@"AdViewController" bundle:bundle];
+    [self.view addSubview:adView.view];
 }
 
 

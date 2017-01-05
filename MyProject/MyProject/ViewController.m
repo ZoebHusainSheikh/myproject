25f5f3682d0009a93385e9bde19f3b8b51ae078f
Zoeb Sheikh <zsheikh@isystango.com>
2017-01-05 17:26:47 +0530

    first commit

---
+++ MyProject/MyProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyProject
+//
+//  Created by Systango on 05/01/17.
+//  Copyright © 2017 Systango. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
